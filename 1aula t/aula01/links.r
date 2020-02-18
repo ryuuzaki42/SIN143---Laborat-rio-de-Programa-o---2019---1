2c23985aa6baa9f32913810777bd87f5bbeb3117
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 10:51:00 -0300

    Update links.r and sin143Aula01.pdf

---
+++ 1aula t/aula01/links.r
@@ -0,0 +1,19 @@
+
+Netbeans atalhos
+https://netbeans.org/kb/73/java/editor-codereference_pt_BR.html
+
+Curso de Java para Iniciantes - Curso em vídeo
+https://www.youtube.com/playlist?list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR
+
+Curso de POO Java - Curso em vídeo 
+https://www.youtube.com/playlist?list=PLHz_AreHm4dkqe2aR0tQK74m8SFe-aGsY
+
+Curso de C++ e POO e QT - CFBCursos
+https://www.youtube.com/playlist?list=PLx4x_zx8csUjczg1qPHavU1vw1IkBcm40
+
+POO em Java - UNIVESP
+https://www.youtube.com/playlist?list=PLxI8Can9yAHewZWSrlhpId71bk5N_W7W1
+
+Canal Descompila (Java, JDBC, JPA, JavaFX)
+https://www.youtube.com/user/CanalSamuelson
+https://www.youtube.com/playlist?list=PLtiJyyRsEC2rhOjmrHqqDRSRMhVGDX6xS
