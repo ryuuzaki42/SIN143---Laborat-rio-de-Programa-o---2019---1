7518ce1653338bd392e44dff72ea430ff3b95393
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 10:51:24 -0300

    Update gitprimer.pdf, introducaoGitHub.pdf, and links.r

---
+++ 1aula t/aula02/git/links.r
@@ -0,0 +1,13 @@
+
+https://rogerdudler.github.io/git-guide/index.pt_BR.html
+
+https://git-scm.com/book/pt-br/v1/Git-Essencial-Obtendo-um-Reposit%C3%B3rio-Git
+
+https://sempreupdate.com.br/os-melhores-git-clientes-para-linux/
+
+https://tableless.com.br/tudo-que-voce-queria-saber-sobre-git-e-github-mas-tinha-vergonha-de-perguntar/
+
+https://git-scm.com/downloads
+https://git-scm.com/downloads/guis
+https://git-scm.com/doc
+https://git-scm.com/book/pt-br/v2
