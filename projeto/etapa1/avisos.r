faff3172192a34b81334ae85447f329c6b56754e
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 10:56:14 -0300

    Update avalicacaoEtapa1.pdf, avisos.r, and 2 more files...

---
+++ projeto/etapa1/avisos.r
@@ -0,0 +1,15 @@
+Anúncios:
+    0 Os alunos são movidos por pontos. No início eu disse que não teria provas e seria rigoroso na correção. Assim não assuste se lhe tirar pontos
+    1 Alguns não entregaram dentro do tempo estipulado
+    2 Alguns não fizeram o que pedi e/ou faltou partes
+    3 Enviaram documentos em vários formatos, na próxima formalizo melhor, deixando mais rígido as regras/normas. Quem ainda não enviou PDF da apresentação/Documento, envie
+    4 Conduta durante a apresentação, chegue no horário, faça silêncio e preste atenção, caso contrário toda equipe pode perder pontos
+    5 Tentem resolver quaisquer desavenças/problemas na equipe. Posso resolver, mas muitos podem perder pontos
+    6 Funcionamento: equipe apresenta e todas as outras dão suas contribuições e críticas (construtivas)
+
+Links:
+    Templates "prontos" para etapa 1 e futuras
+
+    https://github.com/jpeisenbarth/SRS-Tex
+
+    https://www.overleaf.com/latex/templates/requirements-specification-layout/vbrqbjpzcmfy
